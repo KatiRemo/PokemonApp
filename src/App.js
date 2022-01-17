8c5111c5609b3363ff697b46f115fbf25b7d28df
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-01-17 15:04:01 +0200

    API and axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
@@ -7,7 +7,27 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 
 const App = () => {
- const [pokemons, setPokemons] = useState('wow I am state');
+ const [pokemons, setPokemons] = useState([]);
+
+//  useEffect(() => {
+//    axios.get('https://pokeapi.co/api/v2/pokemon')
+//    .then(res => setPokemons(res.data.results));
+//   //  add another axios with new api and promiseAll 
+//  }, []);
+
+useEffect(() => {
+axios.get("https://pokeapi.co/api/v2/pokemon/").then((res) => {
+const fetches = res.data.results.map((p) =>
+axios.get(p.url).then((res) => res.data)
+);
+Promise.all(fetches).then((data) => {
+setPokemons(data);
+});
+});
+}, []);
+
+
+ console.log('state after GET', pokemons);
 
   return (
     <div>
@@ -18,19 +38,27 @@ const App = () => {
       </Navbar>
 
       <Container>
-      <Row xs={2} md={4} lg={5} className="justify-content-between my-5 d-flex gap-3" />
+      <Row 
+      xs={2} 
+      md={4} 
+      lg={5} 
+      className="justify-content-between my-5 d-flex gap-3">
 
-          <Card bg="dark" text="light">
+      {pokemons.map((p) => (
+        <li>{p.name}</li>
+      ))}
+
+          {/* <Card bg="dark" text="light">
             <Card.Img variant="top" src="https://images.unsplash.com/photo-1580655653885-65763b2597d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt='LA'/>
             <Card.Body>
-              <Card.Title>{pokemons}</Card.Title>
+              <Card.Title>LA</Card.Title>
               <Card.Text>
                 Los Angeles (L.A.), officially the City of Los Angeles, is a city in Southern California, in the United States.
               </Card.Text>
               <Button variant="light" size="md">More about LA</Button>
             </Card.Body>
-            </Card>
-
+            </Card> */}
+          </Row>
         </Container>
     </div>
   );
