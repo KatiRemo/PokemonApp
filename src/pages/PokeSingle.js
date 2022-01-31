93a0f510a0b430d836df181fedb6b23bc0deced0
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-01-31 13:00:38 +0200

    updated single

--- src/pages/PokeSingle.js
+++ src/pages/PokeSingle.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
-
-function getPokemon(pokemonName) {
-    return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-}
+import { useParams, useNavigate } from 'react-router-dom';
+import Loader from '../components/Loader';
+import { Button } from 'react-bootstrap';
 
 const PokeSingle = () => {
-    let { pokemonName, image, ability } = useParams();
+    let { pokemonName } = useParams();
+    let navigate = useNavigate();
+    
+    const [pokemon, setPokemon] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .catch(error => {
+            console.log(error);
+        })
+        .then((res) => {
+            setPokemon(res.data);
+            setIsLoading(false);
+        });
+        console.log(pokemon);
+    }, [])
 
     return (
-        <Card bg="danger" text="dark">
-            <Card.Header><strong>{pokemonName}</strong></Card.Header>
-                <Card.Body>
-                  <Card.Img
-                  variant="top" src={image} />
-                  <p>Ability: {ability}</p>
-                  </Card.Body>
-        </Card>
+        <div className='pokemonData'>
+            {isLoading && <Loader />}
+            {!isLoading && (
+            <div className='single'>
+                <h2>{pokemonName}</h2>
+                <img src={pokemon.sprites.other.dream_world.front_default} />
+                <p>Base Experience: {pokemon.base_experience}</p>
+                <p>Height: {pokemon.height}0 cm</p>
+                <p>Weight: {pokemon.weight} kg</p>
+                <p>
+                    Types: {" "}
+                    <ul>
+                    {pokemon.types.map((item) => (
+                        <li key={item.type.name}>{item.type.name}</li>
+                    ))}
+                    </ul>
+                    </p>
+            </div>
+            )}
+                  <Button onClick={() => navigate(-1)} variant="outline-dark" size="sm">
+                      Back to List
+                  </Button>
+        </div>
     );
 };
 
